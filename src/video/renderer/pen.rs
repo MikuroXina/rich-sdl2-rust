2504311dd4b65c5a2919dc998ac91db1f14b1cf1
MikuroXina <ryosukadnak@gmail.com>
2021-01-07 05:24:48 +0900

    feat: Add line and lines and Add derive macros to Color

--- src/video/renderer/pen.rs
+++ src/video/renderer/pen.rs
@@ -1,7 +1,9 @@
+use crate::video::geo::{Line, Point};
 use crate::{bind, Sdl};
 
 use super::Renderer;
 
+#[derive(Debug, Clone)]
 pub struct Color {
     pub r: u8,
     pub g: u8,
@@ -28,6 +30,31 @@ impl<'renderer> Pen<'renderer> {
         let ret = unsafe { bind::SDL_RenderClear(self.renderer.as_ptr()) };
         if ret != 0 {
             Sdl::error_then_panic("Sdl pen clear")
+        }
+    }
+
+    pub fn line(&self, line: Line) {
+        let ret = unsafe {
+            bind::SDL_RenderDrawLine(
+                self.renderer.as_ptr(),
+                line.start.x,
+                line.start.y,
+                line.end.x,
+                line.end.y,
+            )
+        };
+        if ret != 0 {
+            Sdl::error_then_panic("Sdl pen line")
+        }
+    }
+
+    pub fn lines(&self, points: impl IntoIterator<Item = Point>) {
+        let points: Vec<_> = points.into_iter().map(|p| p.into()).collect();
+        let ret = unsafe {
+            bind::SDL_RenderDrawLines(self.renderer.as_ptr(), points.as_ptr(), points.len() as i32)
+        };
+        if ret != 0 {
+            Sdl::error_then_panic("Sdl pen lines")
         }
     }
 }
