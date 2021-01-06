9b3a2ed147b1fc87284b6eabbf60bd8f711cef07
MikuroXina <ryosukadnak@gmail.com>
2021-01-07 00:47:06 +0900

    feat: Add video subsystem

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 mod bind;
 mod sdl;
+mod video;
 
 pub use sdl::Sdl;
+pub use video::Video;
---
+++ src/video.rs
@@ -0,0 +1,26 @@
+use std::marker::PhantomData;
+
+use crate::{bind, Sdl};
+
+pub struct Video<'sdl> {
+    _phantom: PhantomData<&'sdl ()>,
+}
+
+impl<'sdl> Video<'sdl> {
+    pub fn new(_: &'sdl Sdl) -> Self {
+        let ret = unsafe { bind::SDL_InitSubSystem(bind::SDL_INIT_VIDEO) };
+        if ret != 0 {
+            eprintln!("Sdl video error: {}", Sdl::poll_error());
+            panic!("Sdl video initialization failed");
+        }
+        Self {
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<'sdl> Drop for Video<'sdl> {
+    fn drop(&mut self) {
+        unsafe { bind::SDL_QuitSubSystem(bind::SDL_INIT_VIDEO) }
+    }
+}
