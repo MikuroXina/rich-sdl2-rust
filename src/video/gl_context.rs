1f49fbef63ad77690390937594d500d8727191ff
MikuroXina <ryosukadnak@gmail.com>
2021-01-09 01:20:51 +0900

    feat: Add drawable_size

--- src/video/gl_context.rs
+++ src/video/gl_context.rs
@@ -1,6 +1,8 @@
 use std::ffi::c_void;
+use std::mem::MaybeUninit;
 use std::ptr::NonNull;
 
+use crate::geo::Size;
 use crate::window::WindowContextKind;
 use crate::{bind, window::Window};
 
@@ -27,6 +29,21 @@ impl<'window> GlContext<'window> {
 
     pub fn as_ptr(&self) -> *mut c_void {
         self.ctx.as_ptr()
+    }
+
+    pub fn drawable_size(&self) -> Size {
+        let (mut width, mut height) = (MaybeUninit::uninit(), MaybeUninit::uninit());
+        unsafe {
+            bind::SDL_GL_GetDrawableSize(
+                self.window.as_ptr(),
+                width.as_mut_ptr(),
+                height.as_mut_ptr(),
+            )
+        }
+        Size {
+            width: unsafe { width.assume_init() } as u32,
+            height: unsafe { height.assume_init() } as u32,
+        }
     }
 }
 
