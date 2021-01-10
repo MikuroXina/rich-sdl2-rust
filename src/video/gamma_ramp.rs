6f87716c3ed3709c627ea668f6b50cd54f48beb7
MikuroXina <ryosukadnak@gmail.com>
2021-01-10 21:46:01 +0900

    feat: Add constructor to GammaRamp

--- src/video/gamma_ramp.rs
+++ src/video/gamma_ramp.rs
@@ -1,5 +1,15 @@
+use crate::bind;
+
 #[derive(Debug)]
 pub struct GammaRamp(pub Box<[u16; 256]>);
+
+impl GammaRamp {
+    fn new(gamma: f32) -> Self {
+        let mut ramp = Self::default();
+        unsafe { bind::SDL_CalculateGammaRamp(gamma, ramp.0.as_mut_ptr()) }
+        ramp
+    }
+}
 
 impl Default for GammaRamp {
     fn default() -> Self {
