13f55cdf19cd881ea932e59249d361f411b2c66c
MikuroXina <ryosukadnak@gmail.com>
2021-03-06 21:21:39 +0900

    feat: Add KeySymbol

--- src/event/keyboard.rs
+++ src/event/keyboard.rs
@@ -1,3 +1,26 @@
+use key_code::KeyCode;
+use key_mod::KeyMod;
+use scan_code::ScanCode;
+
+use crate::bind;
+
 pub mod key_code;
 pub mod key_mod;
 pub mod scan_code;
+
+#[derive(Debug, Clone)]
+pub struct KeySymbol {
+    scan_code: ScanCode,
+    key_code: KeyCode,
+    key_mod: KeyMod,
+}
+
+impl From<bind::SDL_Keysym> for KeySymbol {
+    fn from(sym: bind::SDL_Keysym) -> Self {
+        Self {
+            scan_code: sym.scancode.into(),
+            key_code: sym.sym.into(),
+            key_mod: KeyMod::from_bits(sym.mod_).unwrap(),
+        }
+    }
+}
