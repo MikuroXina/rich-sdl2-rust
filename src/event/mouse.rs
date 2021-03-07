ebed3a25a65d974dddda80bcc4bf5aa433b3d3cf
MikuroXina <ryosukadnak@gmail.com>
2021-03-07 10:15:15 +0900

    feat: Add MouseMotionEvent

--- src/event/mouse.rs
+++ src/event/mouse.rs
@@ -1,3 +1,6 @@
+use crate::bind;
+use crate::geo::Point;
+
 #[derive(Debug, Clone)]
 pub enum MouseState {
     Left,
@@ -17,6 +20,32 @@ impl MouseState {
             4 => X1,
             5 => X2,
             _ => unreachable!(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct MouseMotionEvent {
+    pub timestamp: u32,
+    pub window_id: u32,
+    pub mouse_id: u32,
+    pub state: MouseState,
+    pub pos: Point,
+    pub move_amount: Point,
+}
+
+impl From<bind::SDL_MouseMotionEvent> for MouseMotionEvent {
+    fn from(raw: bind::SDL_MouseMotionEvent) -> Self {
+        Self {
+            timestamp: raw.timestamp,
+            window_id: raw.windowID,
+            mouse_id: raw.which,
+            state: MouseState::from_bits(raw.state as u8),
+            pos: Point { x: raw.x, y: raw.y },
+            move_amount: Point {
+                x: raw.xrel,
+                y: raw.yrel,
+            },
         }
     }
 }
