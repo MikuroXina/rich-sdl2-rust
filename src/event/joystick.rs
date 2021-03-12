99db2b1cc7f636d9c62e0f5d362770780b98dbdd
MikuroXina <ryosukadnak@gmail.com>
2021-03-13 03:01:44 +0900

    feat: Add JoystickSet

--- src/event/joystick.rs
+++ src/event/joystick.rs
@@ -1,3 +1,4 @@
+use std::os::raw::c_int;
 use std::ptr::NonNull;
 
 use crate::bind;
@@ -17,5 +18,33 @@ impl Joystick {
         let raw = unsafe { bind::SDL_JoystickInstanceID(self.ptr.as_ptr()) };
         debug_assert_ne!(raw, -1);
         JoystickId(raw as u32)
+    }
+}
+pub struct JoystickSet(Vec<Joystick>);
+
+impl JoystickSet {
+    pub fn init() -> Self {
+        let num_joysticks = unsafe {
+            bind::SDL_InitSubSystem(bind::SDL_INIT_JOYSTICK);
+            bind::SDL_NumJoysticks()
+        } as u32;
+        let joysticks = (0..num_joysticks)
+            .map(|index| {
+                let ptr = unsafe { bind::SDL_JoystickOpen(index as c_int) };
+                Joystick {
+                    recognized_index: index,
+                    ptr: NonNull::new(ptr).unwrap(),
+                }
+            })
+            .collect();
+        Self(joysticks)
+    }
+}
+
+impl Drop for JoystickSet {
+    fn drop(&mut self) {
+        for joystick in &self.0 {
+            unsafe { bind::SDL_JoystickClose(joystick.ptr.as_ptr()) }
+        }
     }
 }
