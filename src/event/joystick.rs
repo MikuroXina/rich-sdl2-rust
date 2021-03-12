f9dbd9448937802b99dc32f1b837e7a28357611f
MikuroXina <ryosukadnak@gmail.com>
2021-03-13 03:00:09 +0900

    feat: Add Joystick

--- src/event/joystick.rs
+++ src/event/joystick.rs
@@ -1,1 +1,21 @@
+use std::ptr::NonNull;
+
+use crate::bind;
+
 pub mod power_level;
+
+#[derive(Debug, PartialEq)]
+pub struct JoystickId(u32);
+
+pub struct Joystick {
+    recognized_index: u32,
+    ptr: NonNull<bind::SDL_Joystick>,
+}
+
+impl Joystick {
+    pub fn instance_id(&self) -> JoystickId {
+        let raw = unsafe { bind::SDL_JoystickInstanceID(self.ptr.as_ptr()) };
+        debug_assert_ne!(raw, -1);
+        JoystickId(raw as u32)
+    }
+}
