a2dfd44dedc45a09852d027c1d7f17610e95d81b
MikuroXina <ryosukadnak@gmail.com>
2021-07-18 21:29:23 +0900

    fix: Fix self mutability and name at convertion fn

--- src/video/texture/lock.rs
+++ src/video/texture/lock.rs
@@ -36,7 +36,7 @@ impl<'texture> Lock<'texture> {
         }
     }
 
-    pub fn to_bytes(&self) -> &mut [u8] {
+    pub fn as_bytes(&mut self) -> &mut [u8] {
         unsafe { std::slice::from_raw_parts_mut(self.pixels.as_ptr() as *mut _, self.len) }
     }
 }
