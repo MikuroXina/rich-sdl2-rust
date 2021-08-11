5fb75a6d7b7e1f8049146d8f3357af76c8b537bd
MikuroXina <ryosukadnak@gmail.com>
2021-08-12 07:17:51 +0900

    feat: Add AudioDevice

--- src/audio.rs
+++ src/audio.rs
@@ -1,1 +1,37 @@
+use std::ffi::CStr;
+
+use crate::bind;
+
 pub mod format;
+
+pub struct AudioDevice {
+    name: String,
+}
+
+impl AudioDevice {
+    pub fn all_devices() -> impl Iterator<Item = Self> {
+        devices(false).map(|name| Self { name })
+    }
+}
+
+pub struct CaptureAudioDevice {
+    name: String,
+}
+
+impl CaptureAudioDevice {
+    pub fn all_devices() -> impl Iterator<Item = Self> {
+        devices(true).map(|name| Self { name })
+    }
+}
+
+fn devices(is_capture: bool) -> impl Iterator<Item = String> {
+    let is_capture_raw = if is_capture { 1 } else { 0 };
+    let num_devices = unsafe {
+        bind::SDL_InitSubSystem(bind::SDL_INIT_AUDIO);
+        bind::SDL_GetNumAudioDevices(is_capture_raw)
+    };
+    (0..num_devices).map(move |index| {
+        let cstr = unsafe { CStr::from_ptr(bind::SDL_GetAudioDeviceName(index, is_capture_raw)) };
+        cstr.to_string_lossy().to_string()
+    })
+}
