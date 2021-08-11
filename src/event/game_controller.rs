b96f346876a5d86ff1da49a7d3f3a7880361e110
MikuroXina <ryosukadnak@gmail.com>
2021-08-11 23:14:34 +0900

    feat: Add add_mapping

--- src/event/game_controller.rs
+++ src/event/game_controller.rs
@@ -1,7 +1,10 @@
 use static_assertions::assert_not_impl_all;
-use std::{ffi::CStr, ptr::NonNull};
+use std::{
+    ffi::{CStr, CString},
+    ptr::NonNull,
+};
 
-use crate::bind;
+use crate::{bind, Result, Sdl, SdlError};
 
 pub mod axis;
 pub mod button;
@@ -42,6 +45,32 @@ impl GameControllerSet {
             .map(|ptr| GameController { ptr })
             .collect();
         Self { controls }
+    }
+
+    pub fn add_mapping(string: &str) -> Result<bool> {
+        let cstr = CString::new(string).expect("string must not be empty");
+        let ret = unsafe { bind::SDL_GameControllerAddMapping(cstr.as_ptr()) };
+        if ret == -1 {
+            Err(SdlError::Others { msg: Sdl::error() })
+        } else {
+            Ok(ret == 1)
+        }
+    }
+
+    pub fn add_mapping_from_file(file_name: &str) -> Result<u32> {
+        let cstr = CString::new(file_name).expect("string must not be empty");
+        let read_binary_mode = CStr::from_bytes_with_nul(b"rb\0").unwrap();
+        let ret = unsafe {
+            bind::SDL_GameControllerAddMappingsFromRW(
+                bind::SDL_RWFromFile(cstr.as_ptr(), read_binary_mode.as_ptr()),
+                1,
+            )
+        };
+        if ret < 0 {
+            Err(SdlError::Others { msg: Sdl::error() })
+        } else {
+            Ok(ret as u32)
+        }
     }
 
     pub fn controllers(&self) -> &[GameController] {
