f76be712457b9974dfb9962e98b6c854f03584ba
MikuroXina <ryosukadnak@gmail.com>
2021-08-12 04:37:42 +0900

    feat: Add conversions

--- src/haptic/direction.rs
+++ src/haptic/direction.rs
@@ -1,3 +1,5 @@
+use crate::bind;
+
 pub enum Direction {
     Polar {
         degree_100: i32,
@@ -11,4 +13,46 @@ pub enum Direction {
         z_degree_100: i32,
         elevation_degree_100: i32,
     },
+}
+
+impl Direction {
+    pub(super) fn to_raw(&self) -> bind::SDL_HapticDirection {
+        match *self {
+            Direction::Polar { degree_100 } => bind::SDL_HapticDirection {
+                type_: bind::SDL_HAPTIC_POLAR as u8,
+                dir: [degree_100, 0, 0],
+            },
+            Direction::Cartesian { x, y, z } => bind::SDL_HapticDirection {
+                type_: bind::SDL_HAPTIC_CARTESIAN as u8,
+                dir: [x, y, z],
+            },
+            Direction::Spherical {
+                z_degree_100,
+                elevation_degree_100,
+            } => bind::SDL_HapticDirection {
+                type_: bind::SDL_HAPTIC_SPHERICAL as u8,
+                dir: [z_degree_100, elevation_degree_100, 0],
+            },
+        }
+    }
+}
+
+impl From<bind::SDL_HapticDirection> for Direction {
+    fn from(raw: bind::SDL_HapticDirection) -> Self {
+        match raw.type_ as u32 {
+            bind::SDL_HAPTIC_POLAR => Self::Polar {
+                degree_100: raw.dir[0],
+            },
+            bind::SDL_HAPTIC_CARTESIAN => Self::Cartesian {
+                x: raw.dir[0],
+                y: raw.dir[0],
+                z: raw.dir[0],
+            },
+            bind::SDL_HAPTIC_SPHERICAL => Self::Spherical {
+                z_degree_100: raw.dir[0],
+                elevation_degree_100: raw.dir[1],
+            },
+            _ => unreachable!(),
+        }
+    }
 }
