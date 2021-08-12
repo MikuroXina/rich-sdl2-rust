ec00b5ac9a92dc261e7e7bd7bba6f958695249bb
MikuroXina <ryosukadnak@gmail.com>
2021-08-12 21:32:51 +0900

    feat: Add performance mod

--- src/timer.rs
+++ src/timer.rs
@@ -3,3 +3,15 @@ use crate::{bind, Sdl};
 pub fn delay(ms: u32) {
     unsafe { bind::SDL_Delay(ms) }
 }
+
+pub mod performance {
+    use crate::bind;
+
+    pub fn counter() -> u64 {
+        unsafe { bind::SDL_GetPerformanceCounter() }
+    }
+
+    pub fn frequency() -> u64 {
+        unsafe { bind::SDL_GetPerformanceFrequency() }
+    }
+}
