d1155dc297ff13c3bd2e49d6190867e307a2dc01
MikuroXina <ryosukadnak@gmail.com>
2021-08-12 21:09:43 +0900

    feat: Enforce SdlError

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,24 @@
-#[derive(Debug)]
+use static_assertions::assert_impl_all;
+
+#[derive(Debug, Clone)]
 pub enum SdlError {
     UnsupportedFeature,
     OutOfMemory,
     Others { msg: String },
 }
 
+assert_impl_all!(SdlError: Send, Sync);
+
 pub type Result<T> = std::result::Result<T, SdlError>;
+
+impl std::fmt::Display for SdlError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SdlError::UnsupportedFeature => f.write_str("the feature is unsupported"),
+            SdlError::OutOfMemory => f.write_str("out of memory"),
+            SdlError::Others { msg } => f.write_str(msg),
+        }
+    }
+}
+
+impl std::error::Error for SdlError {}
