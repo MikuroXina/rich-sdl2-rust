5175c2a5833d4366813f7b27966ac548f8946cd3
MikuroXina <ryosukadnak@gmail.com>
2021-08-14 18:26:53 +0900

    feat: Add VkSurface

--- src/video/vulkan.rs
+++ src/video/vulkan.rs
@@ -1,4 +1,5 @@
-use std::{ffi::CStr, os::raw::c_uint};
+use ash::vk::{Handle as _, Instance};
+use std::{ffi::CStr, marker::PhantomData, mem::MaybeUninit, os::raw::c_uint, ptr::NonNull};
 
 use crate::{bind, window::Window, Result, SdlError};
 
@@ -37,5 +38,31 @@ impl<'window> VkInstance<'window> {
 
     pub fn extensions(&self) -> &[String] {
         &self.extensions
+    }
+}
+
+pub struct VkSurface<'vk> {
+    ptr: NonNull<bind::VkSurfaceKHR_T>,
+    _phantom: PhantomData<&'vk VkInstance<'vk>>,
+}
+
+impl<'vk> VkSurface<'vk> {
+    pub fn new(vk: &'vk VkInstance<'vk>, instance: Instance) -> Result<Self> {
+        let mut surface = MaybeUninit::uninit();
+        let ret = unsafe {
+            bind::SDL_Vulkan_CreateSurface(
+                vk.window.as_ptr(),
+                instance.as_raw() as *mut _,
+                surface.as_mut_ptr(),
+            )
+        };
+        if ret == bind::SDL_bool_SDL_FALSE {
+            Err(SdlError::UnsupportedFeature)
+        } else {
+            Ok(Self {
+                ptr: NonNull::new(unsafe { surface.assume_init() }).unwrap(),
+                _phantom: PhantomData,
+            })
+        }
     }
 }
