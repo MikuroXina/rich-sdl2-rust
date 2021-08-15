69f8401decf81b698c9badf50b8ccf24d51beb4e
MikuroXina <ryosukadnak@gmail.com>
2021-08-15 21:35:10 +0900

    doc: Add doc

--- src/event/window.rs
+++ src/event/window.rs
@@ -1,27 +1,51 @@
-use crate::bind;
+//! Events for the window.
 
+use crate::{
+    bind,
+    geo::{Point, Size},
+};
+
+/// The details what occurred in [`WindowEvent`].
 #[derive(Debug, Clone)]
 pub enum WindowEventDetails {
+    /// The window became to be shown.
     Shown,
+    /// The window became to bi hidden.
     Hidden,
+    /// The window was exposed.
     Exposed,
-    Moved { x: i32, y: i32 },
-    Resized { width: u32, height: u32 },
-    SizeChanged { width: u32, height: u32 },
+    /// The windows was moved.
+    Moved(Point),
+    /// The window was resized by manually.
+    Resized(Size),
+    /// The window was resized by programmatically.
+    SizeChanged(Size),
+    /// The window was minimized.
     Minimized,
+    /// The window was maximized.
     Maximized,
+    /// The window was restored from max/min.
     Restored,
+    /// The cursor was entered in the window.
     Enter,
+    /// The cursor was left from the window.
     Leave,
+    /// The window was focused.
     FocusGained,
+    /// The window lost focused.
     FocusLost,
+    /// The window was closed.
     Close,
 }
 
+/// An event on interacting to the window.
 #[derive(Debug, Clone)]
 pub struct WindowEvent {
+    /// When this event occurred.
     pub timestamp: u32,
+    /// The id of the window focused.
     pub window_id: u32,
+    /// The kind of the event.
     pub details: WindowEventDetails,
 }
 
@@ -44,15 +68,17 @@ impl From<bind::SDL_WindowEvent> for WindowEvent {
                 bind::SDL_WindowEventID_SDL_WINDOWEVENT_SHOWN => Shown,
                 bind::SDL_WindowEventID_SDL_WINDOWEVENT_HIDDEN => Hidden,
                 bind::SDL_WindowEventID_SDL_WINDOWEVENT_EXPOSED => Exposed,
-                bind::SDL_WindowEventID_SDL_WINDOWEVENT_MOVED => Moved { x: data1, y: data2 },
-                bind::SDL_WindowEventID_SDL_WINDOWEVENT_RESIZED => Resized {
+                bind::SDL_WindowEventID_SDL_WINDOWEVENT_MOVED => {
+                    Moved(Point { x: data1, y: data2 })
+                }
+                bind::SDL_WindowEventID_SDL_WINDOWEVENT_RESIZED => Resized(Size {
                     width: data1 as u32,
                     height: data2 as u32,
-                },
-                bind::SDL_WindowEventID_SDL_WINDOWEVENT_SIZE_CHANGED => SizeChanged {
+                }),
+                bind::SDL_WindowEventID_SDL_WINDOWEVENT_SIZE_CHANGED => SizeChanged(Size {
                     width: data1 as u32,
                     height: data2 as u32,
-                },
+                }),
                 bind::SDL_WindowEventID_SDL_WINDOWEVENT_MINIMIZED => Minimized,
                 bind::SDL_WindowEventID_SDL_WINDOWEVENT_MAXIMIZED => Maximized,
                 bind::SDL_WindowEventID_SDL_WINDOWEVENT_RESTORED => Restored,
