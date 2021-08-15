4b04102c3b8fb362300a037c73aab4bc23f0de73
MikuroXina <ryosukadnak@gmail.com>
2021-08-15 14:59:19 +0900

    fix: Fix & Add doc

--- src/event/joystick/power_level.rs
+++ src/event/joystick/power_level.rs
@@ -1,14 +1,20 @@
 use crate::bind;
 
+/// A power level of a joystick.
 #[derive(Debug, Clone, Copy)]
 pub enum PowerLevel {
+    /// A power level is unavailable.
     Unknown,
+    /// The power level is a few left.
     Empty,
+    /// The power level is on low.
     Low,
+    /// The power level is on medium.
     Medium,
+    /// The power level is full.
     Full,
+    /// The power is coming from wired.
     Wired,
-    Max,
 }
 
 impl From<bind::SDL_JoystickPowerLevel> for PowerLevel {
@@ -20,7 +26,6 @@ impl From<bind::SDL_JoystickPowerLevel> for PowerLevel {
             bind::SDL_JoystickPowerLevel_SDL_JOYSTICK_POWER_MEDIUM => Medium,
             bind::SDL_JoystickPowerLevel_SDL_JOYSTICK_POWER_FULL => Full,
             bind::SDL_JoystickPowerLevel_SDL_JOYSTICK_POWER_WIRED => Wired,
-            bind::SDL_JoystickPowerLevel_SDL_JOYSTICK_POWER_MAX => Max,
             _ => Unknown,
         }
     }
