06e64ac203c52034407f7e211870bfc9c624896d
MikuroXina <ryosukadnak@gmail.com>
2021-08-16 15:26:54 +0900

    doc: Add doc

--- src/video/clipboard.rs
+++ src/video/clipboard.rs
@@ -1,13 +1,17 @@
+//! Clipboard controls.
+
 use std::ffi::CString;
 
 use crate::bind;
 
+/// A text from the clipboard.
 #[derive(Debug, PartialEq, Eq)]
 pub struct ClipboardText {
     text: String,
 }
 
 impl ClipboardText {
+    /// Get a clipboard text if exists.
     pub fn new() -> Option<Self> {
         let ptr = unsafe { bind::SDL_GetClipboardText() };
         if ptr.is_null() {
@@ -18,10 +22,12 @@ impl ClipboardText {
         Some(Self { text })
     }
 
+    /// Returns a reference to the clipboard string .
     pub fn text(&self) -> &String {
         &self.text
     }
 
+    /// Converts into `String`.
     pub fn into_string(self) -> String {
         self.text
     }
