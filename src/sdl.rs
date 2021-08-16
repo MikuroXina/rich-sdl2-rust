0dc43a2122667c58d30e02a6c6ca740fd9308ef5
MikuroXina <ryosukadnak@gmail.com>
2021-08-16 13:02:16 +0900

    doc: Add doc

--- src/sdl.rs
+++ src/sdl.rs
@@ -3,10 +3,14 @@ use std::{cell::Cell, ffi::CStr, marker::PhantomData};
 
 use crate::bind;
 
+/// A version for SDL2.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct SdlVersion {
+    /// A major number in the version.
     pub major: u8,
+    /// A minor number in the version.
     pub minor: u8,
+    /// A patch number in the version.
     pub patch: u8,
 }
 
@@ -26,6 +30,7 @@ impl From<bind::SDL_version> for SdlVersion {
     }
 }
 
+/// A root controller for SDL2. But some sub-feature does not require `Sdl`.
 pub struct Sdl {
     _phantom: PhantomData<Cell<u8>>,
 }
@@ -33,6 +38,7 @@ pub struct Sdl {
 assert_not_impl_all!(Sdl: Send, Sync);
 
 impl Sdl {
+    /// Setup the SDL2 system.
     pub fn new() -> Self {
         let ret = unsafe {
             bind::SDL_SetMainReady();
@@ -46,6 +52,7 @@ impl Sdl {
         }
     }
 
+    /// Returns the version of SDL2.
     pub fn version() -> SdlVersion {
         use bind::SDL_version;
         let mut ver = SDL_version {
@@ -57,6 +64,7 @@ impl Sdl {
         ver.into()
     }
 
+    /// Returns the revision string.
     pub fn revision_str() -> &'static str {
         let raw_str = unsafe { bind::SDL_GetRevision() };
         unsafe { std::ffi::CStr::from_ptr(raw_str) }
@@ -64,20 +72,24 @@ impl Sdl {
             .expect("Getting revision failed")
     }
 
+    /// REturns the revision number.
     pub fn revision_num() -> u32 {
         (unsafe { bind::SDL_GetRevisionNumber() }) as u32
     }
 
+    /// Gets the platform string.
     pub fn platform() -> &'static str {
         let cstr = unsafe { CStr::from_ptr(bind::SDL_GetPlatform()) };
         cstr.to_str().unwrap()
     }
 
+    /// On an unrecoverable error detected, panics with the provided `context`.
     pub fn error_then_panic(context: &'static str) -> ! {
         eprintln!("{} error: {}", context, Self::error());
         panic!("Unrecoverable Sdl error occurred");
     }
 
+    /// Reads the error string from SDL2.
     pub fn error() -> String {
         let raw_str = unsafe { bind::SDL_GetError() };
         let error = unsafe { std::ffi::CStr::from_ptr(raw_str) }
