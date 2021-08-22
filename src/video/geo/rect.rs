4f47678da403549832a47bf5aa9d6e38b19c13a5
MikuroXina <MikuroXina@users.noreply.github.com>
2021-08-22 18:14:16 +0000

    deploy: 0e610eb7a6163c17dfb93e74d13431f1d151daa6

--- src/video/geo/rect.rs
+++ src/video/geo/rect.rs
@@ -38,6 +38,44 @@ impl From<Rect> for bind::SDL_Rect {
 }
 
 impl Rect {
+    /// Constructs a rect from the center.
+    pub fn from_center(center: Point, size: Size) -> Self {
+        Self {
+            up_left: Point {
+                x: center.x - size.width as i32 / 2,
+                y: center.y - size.height as i32 / 2,
+            },
+            size,
+        }
+    }
+    /// Constructs a rect from the bottom right.
+    pub fn from_bottom_right(bottom_right: Point, size: Size) -> Self {
+        Self {
+            up_left: Point {
+                x: (bottom_right.x - size.width as i32),
+                y: (bottom_right.y - size.height as i32),
+            },
+            size,
+        }
+    }
+
+    /// Constructs a rect from x and y coordinates.
+    pub fn from_xs_ys(mut xs: [i32; 2], mut ys: [i32; 2]) -> Self {
+        if xs[1] < xs[0] {
+            xs.swap(0, 1);
+        }
+        if ys[1] < ys[0] {
+            ys.swap(0, 1);
+        }
+        Self {
+            up_left: Point { x: xs[0], y: ys[0] },
+            size: Size {
+                width: (xs[1] - xs[0]) as u32,
+                height: (ys[1] - ys[0]) as u32,
+            },
+        }
+    }
+
     /// Returns the bottom right point of the rectangle.
     pub fn bottom_right(self) -> Point {
         Point {
@@ -58,6 +96,23 @@ impl Rect {
             x: self.up_left.x,
             y: self.up_left.y + self.size.height as i32,
         }
+    }
+
+    /// Returns the left x of the rect.
+    pub fn left(self) -> i32 {
+        self.up_left.x
+    }
+    /// Returns the right x of the rect.
+    pub fn right(self) -> i32 {
+        self.up_left.x + self.size.width as i32
+    }
+    /// Returns the top y of the rect.
+    pub fn top(self) -> i32 {
+        self.up_left.y
+    }
+    /// Returns the bottom y of the rect.
+    pub fn bottom(self) -> i32 {
+        self.up_left.y + self.size.height as i32
     }
 
     /// Returns the enclosed rectangle of the points, with the clip region.
@@ -99,8 +154,8 @@ impl Rect {
     }
 
     /// Returns whether the rectangle is empty.
-    pub fn empty(&self) -> bool {
-        self.size.width != 0 || self.size.height != 0
+    pub fn is_empty(&self) -> bool {
+        self.size.width == 0 || self.size.height == 0
     }
 
     /// Returns the union of two rectangles.
@@ -114,5 +169,63 @@ impl Rect {
             )
         }
         unsafe { raw.assume_init() }.into()
+    }
+
+    /// Creates a new rect from the rect with overwriting the new top y position.
+    pub fn with_top(self, y: i32) -> Self {
+        Self::from_bottom_right(
+            self.bottom_right(),
+            Size {
+                height: y.saturating_sub(self.bottom()).max(0) as u32,
+                ..self.size
+            },
+        )
+    }
+    /// Creates a new rect from the rect with overwriting the new right x position.
+    pub fn with_right(self, x: i32) -> Self {
+        Self {
+            size: Size {
+                width: x.saturating_sub(self.left()).max(0) as u32,
+                height: self.size.height,
+            },
+            ..self
+        }
+    }
+    /// Creates a new rect from the rect with overwriting the new bottom y position.
+    pub fn with_bottom(self, y: i32) -> Self {
+        Self {
+            size: Size {
+                width: self.size.width,
+                height: self.top().saturating_sub(y) as u32,
+            },
+            ..self
+        }
+    }
+    /// Creates a new rect from the rect with overwriting the new left x position.
+    pub fn with_left(self, x: i32) -> Self {
+        Self::from_bottom_right(
+            self.bottom_right(),
+            Size {
+                width: self.right().saturating_sub(x).max(0) as u32,
+                ..self.size
+            },
+        )
+    }
+
+    /// Creates a new rect that increased the top from the rect.
+    pub fn extend_top(self, increase: u32) -> Self {
+        self.with_top(self.top() - increase as i32)
+    }
+    /// Creates a new rect that increased the right from the rect.
+    pub fn extend_right(self, increase: u32) -> Self {
+        self.with_right(self.right() + increase as i32)
+    }
+    /// Creates a new rect that increased the bottom from the rect.
+    pub fn extend_bottom(self, increase: u32) -> Self {
+        self.with_bottom(self.bottom() + increase as i32)
+    }
+    /// Creates a new rect that increased the left from the rect.
+    pub fn extend_left(self, increase: u32) -> Self {
+        self.with_left(self.left() - increase as i32)
     }
 }
