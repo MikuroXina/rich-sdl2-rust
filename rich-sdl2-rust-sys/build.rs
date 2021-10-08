c12b42ca809efb4e6b28af9f3b1fcc1ff04d4c8e
MikuroXina <ryosukadnak@gmail.com>
2021-10-08 15:42:29 +0900

    build: Fix with set_link

--- rich-sdl2-rust-sys/build.rs
+++ rich-sdl2-rust-sys/build.rs
@@ -12,6 +12,10 @@ fn main() {
         .map(|path| format!("-I{}", path.to_string_lossy()))
         .collect();
 
+    let target = env::var("TARGET").expect("Cargo build scripts always have TARGET");
+    let target_os = target.splitn(3, '-').nth(2).unwrap();
+    set_link(target_os);
+
     println!("cargo:rerun-if-changed=wrapper.h");
 
     let bindings = bindgen::Builder::default()
@@ -31,4 +35,31 @@ fn main() {
     bindings
         .write_to_file(root.join("bind.rs"))
         .expect("writing `bind.rs` failed");
+}
+
+fn set_link(target_os: &str) {
+    if target_os.contains("windows") {
+        println!("cargo:rustc-link-lib=shell32");
+        println!("cargo:rustc-link-lib=user32");
+        println!("cargo:rustc-link-lib=gdi32");
+        println!("cargo:rustc-link-lib=winmm");
+        println!("cargo:rustc-link-lib=imm32");
+        println!("cargo:rustc-link-lib=ole32");
+        println!("cargo:rustc-link-lib=oleaut32");
+        println!("cargo:rustc-link-lib=version");
+        println!("cargo:rustc-link-lib=uuid");
+        println!("cargo:rustc-link-lib=dinput8");
+        println!("cargo:rustc-link-lib=dxguid");
+        println!("cargo:rustc-link-lib=setupapi");
+    } else if target_os == "darwin" {
+        println!("cargo:rustc-link-lib=framework=Cocoa");
+        println!("cargo:rustc-link-lib=framework=IOKit");
+        println!("cargo:rustc-link-lib=framework=Carbon");
+        println!("cargo:rustc-link-lib=framework=ForceFeedback");
+        println!("cargo:rustc-link-lib=framework=CoreVideo");
+        println!("cargo:rustc-link-lib=framework=CoreAudio");
+        println!("cargo:rustc-link-lib=framework=AudioToolbox");
+        println!("cargo:rustc-link-lib=framework=Metal");
+        println!("cargo:rustc-link-lib=iconv");
+    }
 }
