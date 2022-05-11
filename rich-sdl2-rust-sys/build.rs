d725bb482d47701b4b252e883244546b06ec9203
Mikuroさいな <ryosukadnak@gmail.com>
2022-05-11 12:32:26 +0900

    fix: Remove vcpkg (#4)
    * fix: Remove vcpkg
    
    * ci: Remove vcpkg from workflow
    
    * ci: Use vendor feature in CI
    
    * build: Fix about dir
    
    * build: Fix include dir
    
    * build: Improve build process
    
    * build: Add feature guard
    
    * feat: Feature vendor as default
    
    * build: Fix path cloning into
    
    * build: Fix spawning configure
    
    * build: Fix to create repo dir
    
    * build: Fix order of remove and create
    
    * build: Fix to canonicalize configure path
    
    * build: Improve removing repo dir
    
    * build: Fix sdl dir
    
    * build: Fix repo_path control
    
    * build: Avoid with_file_name
    
    * chore: Add debug output
    
    * chore: More debug and specify dir on make
    
    * build: Fix to wait process
    
    * build: Support Windows project
    
    * ci: Fix to add msbuild to PATH
    
    * build: Change default into static
    
    * build: Make git2 as not optional
    
    * build: Move files to dest of installation
    
    * ci: Fix to use vendor feature
    
    * ci: Use newer actions
    
    * build: Fix include install dir
    
    * build: Fix to work in build dir
    
    * build: Use cmake instead of configure
    
    * build: Fix specifying path
    
    * build: Use prefix option
    
    * build: Use CMAKE_INSTALL_PREFIX env
    
    * build: Fix usage of CMAKE_INSTALL_PREFIX
    
    * build: Fix installation path of include
    
    * build: Fix installation on windows
    
    * build: Fix to create dir
    
    * build: Fix to copy lib file
    
    * build: Use create_dir_all
    
    * build: Fix to copy them all
    
    * build: Make dynamic as default
    
    * chore: Log built library
    
    * build: Add specifying dylib
    
    * build: Add project_to_use switch
    
    * build: Fix using msbuild
    
    * chore: Remove verbose
    
    * chore: Bump sys version
    
    * build: Improve build options
    
    * build: Fix to use Debug config
    
    * build: Fix args for msbuild
    
    * build: Prevent rebuild SDL

--- rich-sdl2-rust-sys/build.rs
+++ rich-sdl2-rust-sys/build.rs
@@ -1,13 +1,23 @@
-use std::{env, path::PathBuf};
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
+
+#[cfg(not(any(feature = "static", feature = "dynamic", feature = "vendor")))]
+compile_error!(r#"Either feature "static", "dynamic" or "bar" must be enabled."#);
+
+#[cfg(all(feature = "static", feature = "dynamic"))]
+compile_error!(r#"Feature "static" and "dynamic" cannot coexist."#);
 
 fn main() {
-    let includes: Vec<_> = include_paths()
+    let target = env::var("TARGET").expect("Cargo build scripts always have TARGET");
+    let target_os = target.splitn(3, '-').nth(2).unwrap();
+
+    let includes: Vec<_> = include_paths(target_os)
         .map(|path| format!("-I{}", path.display()))
         .collect();
     eprintln!("{:?}", includes);
 
-    let target = env::var("TARGET").expect("Cargo build scripts always have TARGET");
-    let target_os = target.splitn(3, '-').nth(2).unwrap();
     set_link(target_os);
 
     set_lib_dir();
@@ -33,27 +43,150 @@ fn main() {
         .expect("writing `bind.rs` failed");
 }
 
-fn include_paths() -> impl Iterator<Item = PathBuf> {
-    vcpkg::Config::new()
-        .emit_includes(true)
-        .find_package("sdl2")
+fn include_paths(target_os: &str) -> impl Iterator<Item = PathBuf> {
+    let vendor_include = if cfg!(feature = "vendor") {
+        let root_dir = PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR not found"));
+        let lib_dir = root_dir.join("lib");
+        let include_dir = root_dir.join("include");
+
+        // setup vendored
+        let repo_path = root_dir.join("SDL");
+        if !repo_path.is_dir() {
+            build_vendor(repo_path, target_os, &include_dir, &lib_dir, &root_dir);
+        }
+        println!("cargo:rustc-link-search={}", lib_dir.display());
+        eprintln!("vendored SDL: {}", root_dir.display());
+        vec![include_dir]
+    } else {
+        vec![]
+    };
+    pkg_config::Config::new()
+        .atleast_version("2.0.16")
+        .probe("sdl2")
         .into_iter()
         .flat_map(|sdl2| sdl2.include_paths)
-        .chain(
-            pkg_config::Config::new()
-                .atleast_version("2.0.16")
-                .probe("sdl2")
-                .into_iter()
-                .flat_map(|sdl2| sdl2.include_paths),
-        )
         .chain(std::env::var("SDL2_PATH").map(PathBuf::from).into_iter())
+        .chain(vendor_include.into_iter())
+}
+
+fn build_vendor(
+    repo_path: PathBuf,
+    target_os: &str,
+    include_dir: &Path,
+    lib_dir: &Path,
+    root_dir: &Path,
+) {
+    use git2::Repository;
+    use std::process::Command;
+
+    eprintln!("SDL cloning into: {}", repo_path.display());
+    let _ = std::fs::create_dir_all(&repo_path);
+    if std::fs::remove_dir_all(&repo_path).is_ok() {
+        eprintln!("cleaned SDL repository dir")
+    }
+
+    let url = "https://github.com/libsdl-org/SDL";
+    Repository::clone_recurse(url, &repo_path).expect("failed to clone SDL repository");
+    if target_os.contains("windows") {
+        let target_platform = if cfg!(target_pointer_width = "64") {
+            "Platform=x64"
+        } else {
+            r#"Platform="Any CPU""#
+        };
+        let build = Command::new("msbuild")
+            .arg(format!("/p:Configuration=Debug,{}", target_platform))
+            .arg(repo_path.join("VisualC").join("SDL.sln"))
+            .spawn()
+            .expect("failed to build project");
+        assert!(
+            build
+                .wait_with_output()
+                .expect("build process stopped")
+                .status
+                .success(),
+            "build failed"
+        );
+        let include_install_dir = include_dir.join("SDL2");
+        std::fs::create_dir_all(&include_install_dir).expect("failed to create lib dir");
+        for file in std::fs::read_dir(repo_path.join("include"))
+            .expect("headers not found in repo")
+            .flatten()
+        {
+            let path = file.path();
+            if path.is_file() && path.extension() == Some(std::ffi::OsStr::new("h")) {
+                std::fs::copy(&path, include_install_dir.join(path.file_name().unwrap()))
+                    .expect("failed to copy header file");
+            }
+        }
+        let project_to_use = if cfg!(target_pointer_width = "64") {
+            "x64"
+        } else {
+            "Win32"
+        };
+        std::fs::create_dir_all(lib_dir).expect("failed to create lib dir");
+        for file in std::fs::read_dir(repo_path.join("VisualC").join(project_to_use).join("Debug"))
+            .expect("build dir not found")
+            .flatten()
+        {
+            let path = file.path();
+            if path.is_file() {
+                eprintln!("built library: {}", path.display());
+                std::fs::copy(&path, lib_dir.join(path.file_name().unwrap()))
+                    .expect("failed to copy built library");
+            }
+        }
+    } else {
+        let build_path = repo_path.join("build");
+        std::fs::create_dir(&build_path).expect("failed to mkdir build");
+        let cmake = Command::new("cmake")
+            .current_dir(&build_path)
+            .args([
+                format!("-DCMAKE_INSTALL_PREFIX={}", root_dir.display()),
+                "..".to_string(),
+            ])
+            .spawn()
+            .expect("failed to configure SDL");
+        assert!(
+            cmake
+                .wait_with_output()
+                .expect("cmake process stopped")
+                .status
+                .success(),
+            "cmake failed"
+        );
+        let build = Command::new("make")
+            .current_dir(&build_path)
+            .spawn()
+            .expect("failed to build SDL");
+        assert!(
+            build
+                .wait_with_output()
+                .expect("build process stopped")
+                .status
+                .success(),
+            "build failed"
+        );
+        let setup = Command::new("make")
+            .arg("install")
+            .current_dir(&build_path)
+            .spawn()
+            .expect("failed to setup SDL");
+        assert!(
+            setup
+                .wait_with_output()
+                .expect("setup process stopped")
+                .status
+                .success(),
+            "setup failed"
+        );
+    }
 }
 
 fn set_link(target_os: &str) {
     #[cfg(feature = "static")]
     println!("cargo:rustc-link-lib=static=SDL2");
     #[cfg(feature = "dynamic")]
-    println!("cargo:rustc-link-lib=SDL2");
+    println!("cargo:rustc-link-lib=dylib=SDL2");
 
     if target_os.contains("windows") {
         println!("cargo:rustc-link-lib=shell32");
@@ -78,32 +211,6 @@ fn set_link(target_os: &str) {
         println!("cargo:rustc-link-lib=framework=AudioToolbox");
         println!("cargo:rustc-link-lib=framework=Metal");
         println!("cargo:rustc-link-lib=iconv");
-    }
-    #[cfg(feature = "vendor")]
-    {
-        use git2::Repository;
-        use std::process::Command;
-
-        let root_dir = env::var("OUT_DIR").expect("OUT_DIR not found");
-
-        // setup vendored
-        let url = "https://github.com/libsdl-org/SDL";
-        let mut repo_path = PathBuf::from(root_dir);
-        repo_path.push("vendor");
-        Repository::clone_recurse(url, &repo_path).expect("failed to clone SDL repository");
-        let mut configure = repo_path.clone();
-        configure.push("configure");
-        Command::new(configure)
-            .spawn()
-            .expect("failed to configure SDL");
-        Command::new("make")
-            .current_dir(&repo_path)
-            .spawn()
-            .expect("failed to build SDL");
-        let mut lib_dir = repo_path.clone();
-        lib_dir.push("build");
-        lib_dir.push(".libs");
-        println!("cargo:rustc-link-search={}", lib_dir);
     }
 }
 
