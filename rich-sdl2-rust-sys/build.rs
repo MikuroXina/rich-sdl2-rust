f8aba34ac98ed5582a05c6c1dc676e44624f74a5
MikuroXina <ryosukadnak@gmail.com>
2022-05-11 12:09:25 +0900

    build: Prevent rebuild SDL

--- rich-sdl2-rust-sys/build.rs
+++ rich-sdl2-rust-sys/build.rs
@@ -1,4 +1,7 @@
-use std::{env, path::PathBuf};
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
 
 #[cfg(not(any(feature = "static", feature = "dynamic", feature = "vendor")))]
 compile_error!(r#"Either feature "static", "dynamic" or "bar" must be enabled."#);
@@ -42,115 +45,14 @@ fn main() {
 
 fn include_paths(target_os: &str) -> impl Iterator<Item = PathBuf> {
     let vendor_include = if cfg!(feature = "vendor") {
-        use git2::Repository;
-        use std::process::Command;
-
         let root_dir = PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR not found"));
         let lib_dir = root_dir.join("lib");
         let include_dir = root_dir.join("include");
 
         // setup vendored
-        let url = "https://github.com/libsdl-org/SDL";
         let repo_path = root_dir.join("SDL");
-        eprintln!("SDL cloning into: {}", repo_path.display());
-        let _ = std::fs::create_dir_all(&repo_path);
-        if std::fs::remove_dir_all(&repo_path).is_ok() {
-            eprintln!("cleaned SDL repository dir")
-        }
-        Repository::clone_recurse(url, &repo_path).expect("failed to clone SDL repository");
-        if target_os.contains("windows") {
-            let target_platform = if cfg!(target_pointer_width = "64") {
-                "Platform=x64"
-            } else {
-                r#"Platform="Any CPU""#
-            };
-            let build = Command::new("msbuild")
-                .arg(format!("/p:Configuration=Debug,{}", target_platform))
-                .arg(repo_path.join("VisualC").join("SDL.sln"))
-                .spawn()
-                .expect("failed to build project");
-            assert!(
-                build
-                    .wait_with_output()
-                    .expect("build process stopped")
-                    .status
-                    .success(),
-                "build failed"
-            );
-            let include_install_dir = include_dir.join("SDL2");
-            std::fs::create_dir_all(&include_install_dir).expect("failed to create lib dir");
-            for file in std::fs::read_dir(repo_path.join("include"))
-                .expect("headers not found in repo")
-                .flatten()
-            {
-                let path = file.path();
-                if path.is_file() && path.extension() == Some(std::ffi::OsStr::new("h")) {
-                    std::fs::copy(&path, include_install_dir.join(path.file_name().unwrap()))
-                        .expect("failed to copy header file");
-                }
-            }
-            let project_to_use = if cfg!(target_pointer_width = "64") {
-                "x64"
-            } else {
-                "Win32"
-            };
-            std::fs::create_dir_all(&lib_dir).expect("failed to create lib dir");
-            for file in
-                std::fs::read_dir(repo_path.join("VisualC").join(project_to_use).join("Debug"))
-                    .expect("build dir not found")
-                    .flatten()
-            {
-                let path = file.path();
-                if path.is_file() {
-                    eprintln!("built library: {}", path.display());
-                    std::fs::copy(&path, lib_dir.join(path.file_name().unwrap()))
-                        .expect("failed to copy built library");
-                }
-            }
-        } else {
-            let build_path = repo_path.join("build");
-            std::fs::create_dir(&build_path).expect("failed to mkdir build");
-            let cmake = Command::new("cmake")
-                .current_dir(&build_path)
-                .args([
-                    format!("-DCMAKE_INSTALL_PREFIX={}", root_dir.display()),
-                    "..".to_string(),
-                ])
-                .spawn()
-                .expect("failed to configure SDL");
-            assert!(
-                cmake
-                    .wait_with_output()
-                    .expect("cmake process stopped")
-                    .status
-                    .success(),
-                "cmake failed"
-            );
-            let build = Command::new("make")
-                .current_dir(&build_path)
-                .spawn()
-                .expect("failed to build SDL");
-            assert!(
-                build
-                    .wait_with_output()
-                    .expect("build process stopped")
-                    .status
-                    .success(),
-                "build failed"
-            );
-            let setup = Command::new("make")
-                .arg("install")
-                .current_dir(&build_path)
-                .spawn()
-                .expect("failed to setup SDL");
-            assert!(
-                setup
-                    .wait_with_output()
-                    .expect("setup process stopped")
-                    .status
-                    .success(),
-                "setup failed"
-            );
+        if !repo_path.is_dir() {
+            build_vendor(repo_path, target_os, &include_dir, &lib_dir, &root_dir);
         }
         println!("cargo:rustc-link-search={}", lib_dir.display());
         eprintln!("vendored SDL: {}", root_dir.display());
@@ -165,6 +67,119 @@ fn include_paths(target_os: &str) -> impl Iterator<Item = PathBuf> {
         .flat_map(|sdl2| sdl2.include_paths)
         .chain(std::env::var("SDL2_PATH").map(PathBuf::from).into_iter())
         .chain(vendor_include.into_iter())
+}
+
+fn build_vendor(
+    repo_path: PathBuf,
+    target_os: &str,
+    include_dir: &Path,
+    lib_dir: &Path,
+    root_dir: &Path,
+) {
+    use git2::Repository;
+    use std::process::Command;
+
+    eprintln!("SDL cloning into: {}", repo_path.display());
+    let _ = std::fs::create_dir_all(&repo_path);
+    if std::fs::remove_dir_all(&repo_path).is_ok() {
+        eprintln!("cleaned SDL repository dir")
+    }
+
+    let url = "https://github.com/libsdl-org/SDL";
+    Repository::clone_recurse(url, &repo_path).expect("failed to clone SDL repository");
+    if target_os.contains("windows") {
+        let target_platform = if cfg!(target_pointer_width = "64") {
+            "Platform=x64"
+        } else {
+            r#"Platform="Any CPU""#
+        };
+        let build = Command::new("msbuild")
+            .arg(format!("/p:Configuration=Debug,{}", target_platform))
+            .arg(repo_path.join("VisualC").join("SDL.sln"))
+            .spawn()
+            .expect("failed to build project");
+        assert!(
+            build
+                .wait_with_output()
+                .expect("build process stopped")
+                .status
+                .success(),
+            "build failed"
+        );
+        let include_install_dir = include_dir.join("SDL2");
+        std::fs::create_dir_all(&include_install_dir).expect("failed to create lib dir");
+        for file in std::fs::read_dir(repo_path.join("include"))
+            .expect("headers not found in repo")
+            .flatten()
+        {
+            let path = file.path();
+            if path.is_file() && path.extension() == Some(std::ffi::OsStr::new("h")) {
+                std::fs::copy(&path, include_install_dir.join(path.file_name().unwrap()))
+                    .expect("failed to copy header file");
+            }
+        }
+        let project_to_use = if cfg!(target_pointer_width = "64") {
+            "x64"
+        } else {
+            "Win32"
+        };
+        std::fs::create_dir_all(lib_dir).expect("failed to create lib dir");
+        for file in std::fs::read_dir(repo_path.join("VisualC").join(project_to_use).join("Debug"))
+            .expect("build dir not found")
+            .flatten()
+        {
+            let path = file.path();
+            if path.is_file() {
+                eprintln!("built library: {}", path.display());
+                std::fs::copy(&path, lib_dir.join(path.file_name().unwrap()))
+                    .expect("failed to copy built library");
+            }
+        }
+    } else {
+        let build_path = repo_path.join("build");
+        std::fs::create_dir(&build_path).expect("failed to mkdir build");
+        let cmake = Command::new("cmake")
+            .current_dir(&build_path)
+            .args([
+                format!("-DCMAKE_INSTALL_PREFIX={}", root_dir.display()),
+                "..".to_string(),
+            ])
+            .spawn()
+            .expect("failed to configure SDL");
+        assert!(
+            cmake
+                .wait_with_output()
+                .expect("cmake process stopped")
+                .status
+                .success(),
+            "cmake failed"
+        );
+        let build = Command::new("make")
+            .current_dir(&build_path)
+            .spawn()
+            .expect("failed to build SDL");
+        assert!(
+            build
+                .wait_with_output()
+                .expect("build process stopped")
+                .status
+                .success(),
+            "build failed"
+        );
+        let setup = Command::new("make")
+            .arg("install")
+            .current_dir(&build_path)
+            .spawn()
+            .expect("failed to setup SDL");
+        assert!(
+            setup
+                .wait_with_output()
+                .expect("setup process stopped")
+                .status
+                .success(),
+            "setup failed"
+        );
+    }
 }
 
 fn set_link(target_os: &str) {
