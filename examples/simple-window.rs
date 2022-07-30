5850afe19733369a03d2fc9045ea64679c4a5b09
MikuroXina <ryosukadnak@gmail.com>
2022-07-31 04:52:29 +0900

    chore: Update example

--- examples/simple-window.rs
+++ examples/simple-window.rs
@@ -1,16 +1,31 @@
-use rich_sdl2_rust::{color::Rgb, renderer::pen::Pen, *};
-use std::cell::Cell;
+use rich_sdl2_rust::{
+    color::Rgb,
+    renderer::pen::Pen,
+    ttf::{
+        font::{pen, Font},
+        Ttf,
+    },
+    *,
+};
+use std::cell::{Cell, RefCell};
 
 pub fn main() {
+    let font_file = std::env::args()
+        .skip(1)
+        .next()
+        .expect("please specify font file on arg");
     let sdl = Sdl::new();
+    let ttf = Ttf::new();
+    let font = Font::new(&ttf, &font_file, 20, None).expect("Arial font not found");
     let video = Video::new(&sdl);
     let window = window::WindowBuilder::builder().build().new_window(&video);
     let renderer = renderer::Renderer::new(&window);
 
+    let message = RefCell::new(String::new());
     let exit = Cell::new(false);
     let mut event = EventBox::new(&video);
     event.handle_window(Box::new(|e| {
-        eprintln!("window event: {:?}", e);
+        *message.borrow_mut() = format!("window event: {:?} {:?}", e.timestamp, e.details);
     }));
     event.handle_quit(Box::new(|e| {
         eprintln!("quit event: {}", e.timestamp);
@@ -18,6 +33,7 @@ pub fn main() {
     }));
 
     while !exit.get() {
+        use pen::{FontRenderExt, FontRenderOptions};
         let pen = Pen::new(&renderer);
         pen.set_color(Rgb {
             r: 127,
@@ -25,6 +41,7 @@ pub fn main() {
             b: 0,
         });
         pen.clear();
+        pen.text(&font, &message.borrow(), FontRenderOptions::default());
 
         event.poll();
     }
